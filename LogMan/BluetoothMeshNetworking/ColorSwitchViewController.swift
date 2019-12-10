1022a3d911961bf042878c9f67e4aecca830fac4
Irene Chen <irenechen@dyn-129-236-218-30.dyn.columbia.edu>
2019-12-10 13:09:27 -0500

    WIP adding bluetooth mesh network, device as both central and peripheral

---
+++ LogMan/BluetoothMeshNetworking/ColorSwitchViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ColorSwitchViewController.swift
+//  LogMan
+//
+//  Created by Irene Chen on 12/10/19.
+//  Copyright © 2019 Irene Chen. All rights reserved.
+//
+
+import UIKit
+
+class ColorSwitchViewController: UIViewController {
+
+    @IBOutlet weak var connectionsLabel: UILabel!
+
+    let colorService = ColorService()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colorService.delegate = self
+    }
+
+    @IBAction func redTapped() {
+        self.change(color: .red)
+        colorService.send(colorName: "red")
+    }
+
+    @IBAction func yellowTapped() {
+        self.change(color: .yellow)
+        colorService.send(colorName: "yellow")
+    }
+
+    func change(color : UIColor) {
+        UIView.animate(withDuration: 0.2) {
+            self.view.backgroundColor = color
+        }
+    }
+    
+}
+
+extension ColorSwitchViewController : ColorServiceDelegate {
+
+    func connectedDevicesChanged(manager: ColorService, connectedDevices: [String]) {
+        OperationQueue.main.addOperation {
+            self.connectionsLabel.text = "Connections: \(connectedDevices)"
+        }
+    }
+
+    func colorChanged(manager: ColorService, colorString: String) {
+        OperationQueue.main.addOperation {
+            switch colorString {
+            case "red":
+                self.change(color: .red)
+            case "yellow":
+                self.change(color: .yellow)
+            default:
+                NSLog("%@", "Unknown color value received: \(colorString)")
+            }
+        }
+    }
+
+}
--- LogMan/ColorService.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ColorService.swift
-//  LogMan
-//
-//  Created by Irene Chen on 12/10/19.
-//  Copyright © 2019 Irene Chen. All rights reserved.
-//
-
-import Foundation
